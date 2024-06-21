6b5f640a668973822adb3c28bc757c9bc2404f3d
Aayush Shukla <oreus.aks@gmail.com>
2024-06-21 22:49:25 +0530

    refactor: Update DataManipulator.ts to calculate price ratios and bounds

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,4 +1,3 @@
-import { timeStamp } from 'console';
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
@@ -8,14 +7,14 @@ export interface Row {
   timestamp: Date,
   upper_bound: number,
   lower_bound: number,
-  trigger_alert: number | undefined,
+  trigger_alert : number| undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
-    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+  static generateRow(serverRespond: ServerRespond[]) : Row {
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
     const upperBound = 1 + 0.05;
     const lowerBound = 1 - 0.05;
@@ -23,10 +22,11 @@ export class DataManipulator {
       price_abc: priceABC,
       price_def: priceDEF,
       ratio,
-      timeStamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
-      upper_bound: upperBound,
-      lower_bound: lowerBound,
-      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
-    }
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? 
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound : upperBound,
+      lower_bound : lowerBound,
+      trigger_alert : (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
   }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Table, TableData } from "@finos/perspective";
+import { Table, TableData, perspective_assets } from "@finos/perspective";
 import { ServerRespond } from "./DataStreamer";
 import { DataManipulator } from "./DataManipulator";
 import "./Graph.css";
@@ -41,9 +41,11 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute("view", "y_line");
-      elem.setAttribute("column-pivots", '["stock"]');
       elem.setAttribute("row-pivots", '["timestamp"]');
-      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute(
+        "columns",
+        '["ratio","lower_bound","upper_bound","trigger_alert"]'
+      );
       elem.setAttribute(
         "aggregates",
         JSON.stringify({
